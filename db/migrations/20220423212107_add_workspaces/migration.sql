4e3e9c6b00930a551e89c01b81dbe72bfbdaec37
Juan Azambuja <juan@mimiquate.com>
2022-04-23 18:21:24 -0300

    chore: add workspaces

---
+++ db/migrations/20220423212107_add_workspaces/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Workspace" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+
+    CONSTRAINT "Workspace_pkey" PRIMARY KEY ("id")
+);
